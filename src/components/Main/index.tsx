3927294752f6492a2f1a87e8b85cd81127c974c2
VITOR HUGO GOUVEIA NUNES <vitorhgn100@gmail.com>
2023-11-18 13:21:08 -0400

    feat/criado section qr code emec

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -4,22 +4,26 @@ import "./styles.css";
 import CardBox from "../CardBox";
 
 // Isso deveria vir do banco de dados dinamicamente
-import img1 from "../../../assets/imgCardBox1.svg"
-import img2 from "../../../assets/imgCardBox2.svg"
-import img3 from "../../../assets/imgCardBox3.svg"
+import img1 from "../../../assets/imgCardBox1.svg";
+import img2 from "../../../assets/imgCardBox2.svg";
+import img3 from "../../../assets/imgCardBox3.svg";
+import SectionEMec from "../SectionEMec";
 
 const Main: React.FC = () => {
   return (
-    <div className='section-img-card'>
-      <div className='card-img-principal'>
-        <img src="../../../assets/imgPrincipal.svg" alt="" />
+    <>
+      <div className="section-img-card">
+        <div className="card-img-principal">
+          <img src="../../../assets/imgPrincipal.svg" alt="" />
+        </div>
+        <div className="card-noticias">
+          <CardBox img={img1} />
+          <CardBox img={img2} />
+          <CardBox img={img3} />
+        </div>
       </div>
-      <div className="card-noticias">
-        <CardBox img={img1} />
-        <CardBox img={img2} />
-        <CardBox img={img3} />
-      </div>
-    </div>
+      <SectionEMec />
+    </>
   );
 };
 
---
+++ src/components/SectionEMec/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./styles.css";
+
+const SectionEMec: React.FC = () => {
+  return <div className='section-emec-main'>
+    <div className='section-emec-img'>
+        <img src="../../../assets/imgEmecQrCode.svg" alt="" />
+        <img src="../../../assets/imgAvaliacaoEmec.svg" alt="" />
+        <img src="../../../assets/imgLogoEmec.svg" alt="" />
+    </div>
+  </div>;
+};
+
+export default SectionEMec;
