6c8a8dce1483139fc0d7ff0fa7e758ec1d30b533
VITOR HUGO GOUVEIA NUNES <vitorhgn100@gmail.com>
2023-11-30 22:47:11 -0400

    fix/corrigido importacao de imagem

--- src/components/SectionEMec/index.tsx
+++ src/components/SectionEMec/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 
 import "./styles.css";
 
+import imgEmecQrCode from  "../../../assets/imgEmecQrCode.svg"
+import imgAvaliacaoEmec from  "../../../assets/imgAvaliacaoEmec.svg"
+import imgLogoEmec from  "../../../assets/imgLogoEmec.svg"
+
 const SectionEMec: React.FC = () => {
   return <div className='section-emec-main'>
     <div className='section-emec-img'>
-        <img src="../../../assets/imgEmecQrCode.svg" alt="" />
-        <img src="../../../assets/imgAvaliacaoEmec.svg" alt="" />
-        <img src="../../../assets/imgLogoEmec.svg" alt="" />
+        <img src={imgEmecQrCode} alt="" />
+        <img src={imgAvaliacaoEmec} alt="" />
+        <img src={imgLogoEmec} alt="" />
     </div>
   </div>;
 };
